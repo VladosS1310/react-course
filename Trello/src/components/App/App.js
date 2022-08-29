d3f9611a485ae718dc5f266ba485dd5698a5197a
VladosS1310 <vladik.prindyuk007@gmail.com>
2022-08-29 17:32:43 +0300

    added HW-3

---
+++ Trello/src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Authorization from "../Authorization/Authorization";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Authorization/>
+    </div>
+  );
+}
+
+export default App;
